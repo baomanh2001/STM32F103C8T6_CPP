f2874c14f745454023bad108da43b060ef12f1cb
baomanh2001 <75458250+baomanh2001@users.noreply.github.com>
2025-06-25 14:28:01 +0700

    Add files via upload

---
+++ CPP/Src/CPPMain.cpp
@@ -0,0 +1,18 @@
+/*
+ * CPPMain.cpp
+ *
+ *  Created on: Jun 25, 2025
+ *      Author: DELL
+ */
+
+#include "main.h"
+
+void CPPMain()
+{
+	while (1)
+	{
+
+	}
+}
+
+
---
+++ Debug/CPP/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../CPP/Src/CPPMain.cpp 
+
+OBJS += \
+./CPP/Src/CPPMain.o 
+
+CPP_DEPS += \
+./CPP/Src/CPPMain.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+CPP/Src/%.o CPP/Src/%.su: ../CPP/Src/%.cpp CPP/Src/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m3 -std=gnu++14 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -IC:/Users/DELL/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.6/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -IC:/Users/DELL/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.6/Drivers/STM32F1xx_HAL_Driver/Inc -IC:/Users/DELL/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.6/Drivers/CMSIS/Device/ST/STM32F1xx/Include -IC:/Users/DELL/STM32Cube/Repository/STM32Cube_FW_F1_V1.8.6/Drivers/CMSIS/Include -I"D:/code/Code STM/Cplusplus_Stm32F103C8T6/CPP/Inc" -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-CPP-2f-Src
+
+clean-CPP-2f-Src:
+	-$(RM) ./CPP/Src/CPPMain.d ./CPP/Src/CPPMain.o ./CPP/Src/CPPMain.su
+
+.PHONY: clean-CPP-2f-Src
+
